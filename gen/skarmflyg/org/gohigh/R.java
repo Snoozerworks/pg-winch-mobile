6f66ffc88eda0b4127bb53df9842d29ea711f551
Markus <markus.bagge@gmail.com>
2014-07-13 17:08:32 +0200

    forgot file

--- gen/skarmflyg/org/gohigh/R.java
+++ gen/skarmflyg/org/gohigh/R.java
@@ -9,10 +9,10 @@ package skarmflyg.org.gohigh;
 
 public final class R {
     public static final class array {
-        public static final int modes=0x7f050000;
+        public static final int modes=0x7f070000;
     }
     public static final class attr {
-        /** <p>Must be a floating point value, such as "<code>1.2</code>".
+        /** <p>Must be an integer value, such as "<code>100</code>".
 <p>This may also be a reference to a resource (in the form
 "<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
 theme attribute (in the form
@@ -28,7 +28,7 @@ theme attribute (in the form
 containing a value of this type.
          */
         public static final int needle_max_val=0x7f01000b;
-        /** <p>Must be a floating point value, such as "<code>1.2</code>".
+        /** <p>Must be an integer value, such as "<code>100</code>".
 <p>This may also be a reference to a resource (in the form
 "<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
 theme attribute (in the form
@@ -52,7 +52,7 @@ theme attribute (in the form
 containing a value of this type.
          */
         public static final int num_format=0x7f010000;
-        /** <p>Must be a floating point value, such as "<code>1.2</code>".
+        /** <p>Must be an integer value, such as "<code>100</code>".
 <p>This may also be a reference to a resource (in the form
 "<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
 theme attribute (in the form
@@ -76,7 +76,7 @@ theme attribute (in the form
 containing a value of this type.
          */
         public static final int raw_lo=0x7f010002;
-        /** <p>Must be a floating point value, such as "<code>1.2</code>".
+        /** <p>Must be an integer value, such as "<code>100</code>".
 <p>This may also be a reference to a resource (in the form
 "<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
 theme attribute (in the form
@@ -84,7 +84,7 @@ theme attribute (in the form
 containing a value of this type.
          */
         public static final int val_hi=0x7f010003;
-        /** <p>Must be a floating point value, such as "<code>1.2</code>".
+        /** <p>Must be an integer value, such as "<code>100</code>".
 <p>This may also be a reference to a resource (in the form
 "<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
 theme attribute (in the form
@@ -119,31 +119,35 @@ containing a value of this type.
         public static final int custom_pump_icon=0x7f020006;
         public static final int custom_temp_icon=0x7f020007;
         public static final int ic_launcher=0x7f020008;
-        public static final int ic_stat_notify=0x7f020009;
+        public static final int ic_menu_cancel=0x7f020009;
+        public static final int ic_menu_play=0x7f02000a;
+        public static final int ic_menu_record=0x7f02000b;
+        public static final int ic_menu_stop=0x7f02000c;
+        public static final int ic_stat_notify=0x7f02000d;
     }
     public static final class id {
-        public static final int LinearLayout1=0x7f070000;
-        public static final int btn_connect=0x7f07000c;
-        public static final int btn_down=0x7f070013;
-        public static final int btn_log=0x7f07000d;
-        public static final int btn_op_load=0x7f07000e;
-        public static final int btn_set=0x7f070012;
-        public static final int btn_settings_act=0x7f07000f;
-        public static final int btn_up=0x7f070014;
-        public static final int chart=0x7f070015;
-        public static final int drum_spd=0x7f070004;
-        public static final int editValue=0x7f070011;
-        public static final int imageView1=0x7f070007;
-        public static final int imageView2=0x7f070008;
-        public static final int imageView3=0x7f070009;
-        public static final int meter1=0x7f070001;
-        public static final int pump_spd=0x7f070005;
-        public static final int sv_log=0x7f07000a;
-        public static final int tableRow1=0x7f070002;
-        public static final int tableRow2=0x7f070006;
-        public static final int temperature=0x7f070003;
-        public static final int txt_log=0x7f07000b;
-        public static final int txt_param_lbl=0x7f070010;
+        public static final int LinearLayout1=0x7f080000;
+        public static final int btn_connect=0x7f08000c;
+        public static final int btn_down=0x7f080013;
+        public static final int btn_log=0x7f08000d;
+        public static final int btn_op_load=0x7f08000e;
+        public static final int btn_set=0x7f080012;
+        public static final int btn_settings_act=0x7f08000f;
+        public static final int btn_up=0x7f080014;
+        public static final int chart=0x7f080015;
+        public static final int drum_spd=0x7f080004;
+        public static final int editValue=0x7f080011;
+        public static final int force_meter=0x7f080001;
+        public static final int imageView1=0x7f080007;
+        public static final int imageView2=0x7f080008;
+        public static final int imageView3=0x7f080009;
+        public static final int pump_spd=0x7f080005;
+        public static final int sv_log=0x7f08000a;
+        public static final int tableRow1=0x7f080002;
+        public static final int tableRow2=0x7f080006;
+        public static final int temperature=0x7f080003;
+        public static final int txt_log=0x7f08000b;
+        public static final int txt_param_lbl=0x7f080010;
     }
     public static final class layout {
         public static final int activity_connect=0x7f030000;
@@ -151,40 +155,51 @@ containing a value of this type.
         public static final int activity_sample=0x7f030002;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int bt_connected=0x7f040017;
-        public static final int bt_disabled=0x7f040016;
-        public static final int bt_disconnected=0x7f040018;
-        public static final int bt_not_supported=0x7f040015;
-        public static final int btn_connect_off=0x7f040004;
-        public static final int btn_connect_on=0x7f040003;
-        public static final int btn_down=0x7f040007;
-        public static final int btn_log_off=0x7f04000a;
-        public static final int btn_log_on=0x7f040009;
-        public static final int btn_op_settings=0x7f040008;
-        public static final int btn_set=0x7f040005;
-        public static final int btn_settings_act=0x7f04000b;
-        public static final int btn_sync_off=0x7f04000d;
-        public static final int btn_sync_on=0x7f04000c;
-        public static final int btn_up=0x7f040006;
-        public static final int btservice_connected=0x7f040011;
-        public static final int btservice_disconnected=0x7f040012;
-        public static final int btservice_label=0x7f040010;
-        public static final int btservice_started=0x7f04000e;
-        public static final int btservice_stopped=0x7f04000f;
-        public static final int img_drum_speed=0x7f04001b;
-        public static final int img_oil_temp=0x7f04001a;
-        public static final int img_pump_speed=0x7f04001c;
-        public static final int menu_settings=0x7f040001;
-        public static final int meter_scale=0x7f040014;
-        public static final int msg_connect_to_winch=0x7f04001d;
-        public static final int parameter_act=0x7f040019;
-        public static final int title_activity_connect=0x7f040002;
-        public static final int title_activity_service_note=0x7f040013;
-        public static final int txt_param_lbl=0x7f04001e;
+        public static final int app_name=0x7f050007;
+        public static final int bt_disabled=0x7f05001d;
+        public static final int bt_not_supported=0x7f05001c;
+        public static final int btn_connect_off=0x7f05000b;
+        public static final int btn_connect_on=0x7f05000a;
+        public static final int btn_down=0x7f05000e;
+        public static final int btn_log_off=0x7f050011;
+        public static final int btn_log_on=0x7f050010;
+        public static final int btn_op_settings=0x7f05000f;
+        public static final int btn_set=0x7f05000c;
+        public static final int btn_settings_act=0x7f050012;
+        public static final int btn_sync_off=0x7f050014;
+        public static final int btn_sync_on=0x7f050013;
+        public static final int btn_up=0x7f05000d;
+        public static final int btservice_connected=0x7f050018;
+        public static final int btservice_disconnected=0x7f050019;
+        public static final int btservice_label=0x7f050017;
+        public static final int btservice_started=0x7f050015;
+        public static final int btservice_stopped=0x7f050016;
+        public static final int img_drum_speed=0x7f050020;
+        public static final int img_oil_temp=0x7f05001f;
+        public static final int img_pump_speed=0x7f050021;
+        public static final int menu_settings=0x7f050008;
+        public static final int meter_scale=0x7f05001b;
+        public static final int msg_connect_to_winch=0x7f050022;
+        /**      <string name="bt_connected">Blåtand ansluten.</string> 
+     <string name="bt_disconnected">Blåtand frånkopplad.</string> 
+         */
+        public static final int parameter_act=0x7f05001e;
+        public static final int state_connected=0x7f050000;
+        public static final int state_disconnected=0x7f050001;
+        public static final int state_log_off=0x7f050006;
+        public static final int state_log_on=0x7f050005;
+        public static final int state_sampels=0x7f050002;
+        public static final int state_stopped=0x7f050004;
+        public static final int state_syncs=0x7f050003;
+        public static final int title_activity_connect=0x7f050009;
+        public static final int title_activity_service_note=0x7f05001a;
+        public static final int txt_param_lbl=0x7f050023;
     }
     public static final class style {
         public static final int AppTheme=0x7f060000;
+    }
+    public static final class xml {
+        public static final int plf=0x7f040000;
     }
     public static final class styleable {
         /** Attributes that can be used with a Digits.
@@ -234,7 +249,7 @@ containing a value of this type.
           attribute's value can be found in the {@link #Digits} array.
 
 
-          <p>Must be a floating point value, such as "<code>1.2</code>".
+          <p>Must be an integer value, such as "<code>100</code>".
 <p>This may also be a reference to a resource (in the form
 "<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
 theme attribute (in the form
@@ -276,7 +291,7 @@ containing a value of this type.
           attribute's value can be found in the {@link #Digits} array.
 
 
-          <p>Must be a floating point value, such as "<code>1.2</code>".
+          <p>Must be an integer value, such as "<code>100</code>".
 <p>This may also be a reference to a resource (in the form
 "<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
 theme attribute (in the form
@@ -290,7 +305,7 @@ containing a value of this type.
           attribute's value can be found in the {@link #Digits} array.
 
 
-          <p>Must be a floating point value, such as "<code>1.2</code>".
+          <p>Must be an integer value, such as "<code>100</code>".
 <p>This may also be a reference to a resource (in the form
 "<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
 theme attribute (in the form
@@ -369,7 +384,7 @@ containing a value of this type.
           attribute's value can be found in the {@link #Meter} array.
 
 
-          <p>Must be a floating point value, such as "<code>1.2</code>".
+          <p>Must be an integer value, such as "<code>100</code>".
 <p>This may also be a reference to a resource (in the form
 "<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
 theme attribute (in the form
@@ -397,7 +412,7 @@ containing a value of this type.
           attribute's value can be found in the {@link #Meter} array.
 
 
-          <p>Must be a floating point value, such as "<code>1.2</code>".
+          <p>Must be an integer value, such as "<code>100</code>".
 <p>This may also be a reference to a resource (in the form
 "<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
 theme attribute (in the form
@@ -439,7 +454,7 @@ containing a value of this type.
           attribute's value can be found in the {@link #Meter} array.
 
 
-          <p>Must be a floating point value, such as "<code>1.2</code>".
+          <p>Must be an integer value, such as "<code>100</code>".
 <p>This may also be a reference to a resource (in the form
 "<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
 theme attribute (in the form
@@ -481,7 +496,7 @@ containing a value of this type.
           attribute's value can be found in the {@link #Meter} array.
 
 
-          <p>Must be a floating point value, such as "<code>1.2</code>".
+          <p>Must be an integer value, such as "<code>100</code>".
 <p>This may also be a reference to a resource (in the form
 "<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
 theme attribute (in the form
@@ -495,7 +510,7 @@ containing a value of this type.
           attribute's value can be found in the {@link #Meter} array.
 
 
-          <p>Must be a floating point value, such as "<code>1.2</code>".
+          <p>Must be an integer value, such as "<code>100</code>".
 <p>This may also be a reference to a resource (in the form
 "<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
 theme attribute (in the form
